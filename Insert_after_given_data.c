6659809993a0d767694e066450e93312d4cddf1a
amenamunny <moriumtinny@gmail.com>
2025-12-09 00:11:08 +0600

    Add files via upload

---
+++ Insert_after_given_data.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+int main() {
+    int LA[] = {10, 20, 30, 40, 50};
+    int n = 5;
+    int data, value, pos = -1;
+
+    printf("Enter data after which to insert: ");
+    scanf("%d", &data);
+    printf("Enter value to insert: ");
+    scanf("%d", &value);
+
+    for(int i=0; i<n; i++) {
+        if(LA[i] == data) {
+            pos = i + 1; // insert after found index
+            break;
+        }
+    }
+
+    if(pos == -1) {
+        printf("Data not found!");
+        return 0;
+    }
+
+    for(int i=n; i>pos; i--)
+        LA[i] = LA[i-1];
+
+    LA[pos] = value;
+    n++;
+
+    printf("Array after insertion:");
+    for(int i=0; i<n; i++)
+        printf("%d ", LA[i]);
+
+    return 0;
+}
